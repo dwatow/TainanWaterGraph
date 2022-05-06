6e4ad06c39154eebaa58491ad8727f5a20102576
chris <dwatow@gmail.com>
2022-05-06 23:56:27 +0800

    add assetsDir, remove publicDir

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicDir: 'TainanWaterGraph',
   build: {
+    assetsDir: 'TainanWaterGraph',
     outDir: 'docs'
   }
 })
